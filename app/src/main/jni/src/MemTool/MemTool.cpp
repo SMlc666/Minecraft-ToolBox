85af0c5e08b9fd1aca5ddb2ec13f5f283ec8c536
SHENMO <qq1030452538@outlook.com>
2025-03-01 20:49:37 +0800

    refactor(MemTool): 重构 Hook 类实现并增强健壮性

--- app/src/main/jni/src/MemTool/MemTool.cpp
+++ app/src/main/jni/src/MemTool/MemTool.cpp
@@ -5,17 +5,35 @@
 #include "MemTool.hpp"
 #include "shadowhook/shadowhook.h"
 #include "Init/Init.hpp"
-Init init_MemTool("init_MemTool",[](){
-    int code = shadowhook_init(shadowhook_mode_t::SHADOWHOOK_MODE_UNIQUE,false);
-    if (code!= SHADOWHOOK_ERRNO_OK) {
-        throw std::runtime_error("shadowhook init failed");
-    }
+[[maybe_unused]] Init init_MemTool("init_MemTool", []() {
+  int code = shadowhook_init(shadowhook_mode_t::SHADOWHOOK_MODE_UNIQUE, false);
+  if (code != SHADOWHOOK_ERRNO_OK) {
+    throw std::runtime_error("shadowhook init failed");
+  }
 });
-MemTool::Hook::Hook() {}
-MemTool::Hook::Hook(void *oldAddr,void **newAddr) {
-
+[[maybe_unused]] MemTool::Hook::Hook(void *funcAddr, void *newAddr, bool autoDestroy)
+    : mAutoDestroy(autoDestroy), mNewAddr(newAddr), mFuncAddr(funcAddr) {
+  if (funcAddr == nullptr || newAddr == nullptr) {
+    throw std::runtime_error("oldAddr or newAddr is nullptr");
+  }
+  mStub = shadowhook_hook_func_addr(funcAddr, newAddr, &mOldAddr);
+  if (mStub == nullptr) {
+    throw std::runtime_error("hook failed");
+  }
 }
-MemTool::Hook::Hook(std::string &libName,std::string &symName,void** newAddr) {
-
+[[maybe_unused]] MemTool::Hook::Hook(std::string &libName, std::string &symName, void *newAddr,
+                                     bool autoDestroy)
+    : mAutoDestroy(autoDestroy), mNewAddr(newAddr) {
+  if (newAddr == nullptr) {
+    throw std::runtime_error("newAddr is nullptr");
+  }
+  mStub = shadowhook_hook_sym_name(libName.c_str(), symName.c_str(), newAddr, &mOldAddr);
+  if (mStub == nullptr) {
+    throw std::runtime_error("hook failed");
+  }
 }
-MemTool::Hook::~Hook() {}
+MemTool::Hook::~Hook() {
+  if (mAutoDestroy) {
+    shadowhook_unhook(mStub);
+  }
+}
--- app/src/main/jni/src/MemTool/MemTool.hpp
+++ app/src/main/jni/src/MemTool/MemTool.hpp
@@ -6,23 +6,36 @@
 #define MBLOADER_MEMTOOL_HPP
 #include <string>
 namespace MemTool {
-    class Hook {
-        public:
-            Hook();
-            Hook(void* oldAddr,void** newAddr);
-            Hook(std::string &libName,std::string &symName,void** newAddr);
-            ~Hook();
-            template<typename T,typename... Args>
-            inline T callOld(Args... args){
-                return reinterpret_cast<T(*)(Args...)>(mOldAddr)(args...);
-            }
-        private:
-            void* mOldAddr;
-            void** mNewAddr;
+class [[maybe_unused]] Hook {
+public:
+  Hook() = default;
+  [[maybe_unused]] Hook(void *funcAddr, void *newAddr, bool autoDestroy = true);
+  [[maybe_unused]] Hook(std::string &libName, std::string &symName, void *newAddr,
+                        bool autoDestroy = true);
+  ~Hook();
+  template <typename T, typename... Args> [[maybe_unused]] T callOld(Args... args) {
+    return reinterpret_cast<T (*)(Args...)>(mOldAddr)( // NOLINT(*-pro-type-reinterpret-cast)
+        args...);
+  }
+  Hook(const Hook &) = delete;
+  Hook &operator=(const Hook &) = delete;
+  Hook(Hook &&) = delete;
+  Hook &operator=(Hook &&) = delete;
+  template <typename T = void *> T getOldAddr() const {
+    return reinterpret_cast<T>(mOldAddr); // NOLINT(*-pro-type-reinterpret-cast)
+  }
+  template <typename T = void **> T getNewAddr() const {
+    return reinterpret_cast<T>(mNewAddr); // NOLINT(*-pro-type-reinterpret-cast)
+  }
 
-    };
+private:
+  void *mOldAddr = nullptr;
+  [[maybe_unused]] void *mNewAddr = nullptr;
+  void *mFuncAddr = nullptr;
+  bool mAutoDestroy = true;
+  void *mStub = nullptr;
+};
 
-}// namespace MemTool
-
+} // namespace MemTool
 
 #endif //MBLOADER_MEMTOOL_HPP
