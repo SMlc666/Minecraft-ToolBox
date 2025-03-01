61876b1f43015806aca94f90330447427554aa84
SHENMO <qq1030452538@outlook.com>
2025-03-01 21:16:33 +0800

    refactor: 更新shadowhook库引用并重构Hook类

--- app/src/main/jni/src/MemTool/MemTool.cpp
+++ app/src/main/jni/src/MemTool/MemTool.cpp
@@ -3,7 +3,7 @@
 //
 
 #include "MemTool.hpp"
-#include "shadowhook/shadowhook.h"
+#include <shadowhook.h>
 #include "Init/Init.hpp"
 [[maybe_unused]] Init init_MemTool("init_MemTool", []() {
   int code = shadowhook_init(shadowhook_mode_t::SHADOWHOOK_MODE_UNIQUE, false);
@@ -11,27 +11,6 @@
     throw std::runtime_error("shadowhook init failed");
   }
 });
-[[maybe_unused]] MemTool::Hook::Hook(void *funcAddr, void *newAddr, bool autoDestroy)
-    : mAutoDestroy(autoDestroy), mNewAddr(newAddr), mFuncAddr(funcAddr) {
-  if (funcAddr == nullptr || newAddr == nullptr) {
-    throw std::runtime_error("oldAddr or newAddr is nullptr");
-  }
-  mStub = shadowhook_hook_func_addr(funcAddr, newAddr, &mOldAddr);
-  if (mStub == nullptr) {
-    throw std::runtime_error("hook failed");
-  }
-}
-[[maybe_unused]] MemTool::Hook::Hook(std::string &libName, std::string &symName, void *newAddr,
-                                     bool autoDestroy)
-    : mAutoDestroy(autoDestroy), mNewAddr(newAddr) {
-  if (newAddr == nullptr) {
-    throw std::runtime_error("newAddr is nullptr");
-  }
-  mStub = shadowhook_hook_sym_name(libName.c_str(), symName.c_str(), newAddr, &mOldAddr);
-  if (mStub == nullptr) {
-    throw std::runtime_error("hook failed");
-  }
-}
 MemTool::Hook::~Hook() {
   if (mAutoDestroy) {
     shadowhook_unhook(mStub);
--- app/src/main/jni/src/MemTool/MemTool.hpp
+++ app/src/main/jni/src/MemTool/MemTool.hpp
@@ -5,13 +5,35 @@
 #ifndef MBLOADER_MEMTOOL_HPP
 #define MBLOADER_MEMTOOL_HPP
 #include <string>
+#include <shadowhook.h>
 namespace MemTool {
 class [[maybe_unused]] Hook {
 public:
   Hook() = default;
-  [[maybe_unused]] Hook(void *funcAddr, void *newAddr, bool autoDestroy = true);
-  [[maybe_unused]] Hook(std::string &libName, std::string &symName, void *newAddr,
-                        bool autoDestroy = true);
+  template <typename T>
+  [[maybe_unused]] Hook(void *funcAddr, T *newAddr, bool autoDestroy = true)
+      : mAutoDestroy(autoDestroy), mNewAddr(reinterpret_cast<void *>(newAddr)),
+        mFuncAddr(reinterpret_cast<void *>(funcAddr)) {
+    if (mFuncAddr == nullptr || mNewAddr == nullptr) {
+      throw std::runtime_error("oldAddr or newAddr is nullptr");
+    }
+    mStub = shadowhook_hook_func_addr(mFuncAddr, mNewAddr, &mOldAddr);
+    if (mStub == nullptr) {
+      throw std::runtime_error("hook failed");
+    }
+  }
+  template <typename T>
+  [[maybe_unused]] Hook(const std::string &libName, const std::string &symName, T *newAddr,
+                        bool autoDestroy = true)
+      : mAutoDestroy(autoDestroy), mNewAddr(reinterpret_cast<void *>(newAddr)) {
+    if (newAddr == nullptr) {
+      throw std::runtime_error("newAddr is nullptr");
+    }
+    mStub = shadowhook_hook_sym_name(libName.c_str(), symName.c_str(), mNewAddr, &mOldAddr);
+    if (mStub == nullptr) {
+      throw std::runtime_error("hook failed");
+    }
+  }
   ~Hook();
   template <typename T, typename... Args> [[maybe_unused]] T callOld(Args... args) {
     return reinterpret_cast<T (*)(Args...)>(mOldAddr)( // NOLINT(*-pro-type-reinterpret-cast)
@@ -19,13 +41,16 @@ public:
   }
   Hook(const Hook &) = delete;
   Hook &operator=(const Hook &) = delete;
-  Hook(Hook &&) = delete;
-  Hook &operator=(Hook &&) = delete;
+  Hook(Hook &&) = default;
+  Hook &operator=(Hook &&) = default;
   template <typename T = void *> T getOldAddr() const {
     return reinterpret_cast<T>(mOldAddr); // NOLINT(*-pro-type-reinterpret-cast)
   }
   template <typename T = void **> T getNewAddr() const {
     return reinterpret_cast<T>(mNewAddr); // NOLINT(*-pro-type-reinterpret-cast)
+  }
+  template <typename T = void *> T getFuncAddr() const {
+    return reinterpret_cast<T>(mFuncAddr); // NOLINT(*-pro-type-reinterpret-cast)
   }
 
 private:
